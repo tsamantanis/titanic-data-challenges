871462fc2cc59461506fdd3cfe85b0752ac70af8
Mitchell Hudson <soggybag@gmail.com>
2021-01-08 15:18:34 -0800

    Update challenges-2.test.js

--- challenges-2.test.js
+++ challenges-2.test.js
@@ -1,5 +1,4 @@
-const index = require('./index.js')
-
+const index = require('./challenges-2.js')
 const fs = require('fs');
 
 const json_data = './titanic-passengers.json'
@@ -24,118 +23,123 @@ beforeAll((done) => {
 	})
 })
 
-test('Test getAll', () => {
-	const allFares = data.map(p => p.fields.fare)
-	const allPclass = data.map(p => p.fields.pclass)
-	const allEmbarked = data.map(p => p.fields.embarked)
 
-	expect(index.getAllValuesForProperty(data, 'fare')).toEqual(allFares)
-	expect(index.getAllValuesForProperty(data, 'pclass')).toEqual(allPclass)
-	expect(index.getAllValuesForProperty(data, 'embarked')).toEqual(allEmbarked)
-})
+describe.skip('Challenge 2 Titanic', () => {
 
-test('Test filterByProperty', () => {
-	const allMale = data.filter(p => p.fields.sex === 'male')
-	const allFemale = data.filter(p => p.fields.sex === 'female')
+	test('Test getAll', () => {
+		const allFares = data.map(p => p.fields.fare)
+		const allPclass = data.map(p => p.fields.pclass)
+		const allEmbarked = data.map(p => p.fields.embarked)
 
-	expect(index.filterByProperty(data, 'sex', 'male')).toEqual(allMale)
-	expect(index.filterByProperty(data, 'sex', 'female')).toEqual(allFemale)
-})
+		expect(index.getAllValuesForProperty(data, 'fare')).toEqual(allFares)
+		expect(index.getAllValuesForProperty(data, 'pclass')).toEqual(allPclass)
+		expect(index.getAllValuesForProperty(data, 'embarked')).toEqual(allEmbarked)
+	})
 
-test('Test filterNullForProperty', () => {
-	const faresNotNull = data.filter(p => p.fields.fare !== undefined)
-	const agesNotNull = data.filter(p => p.fields.age !== undefined)
-	const pclassNotNull = data.filter(p => p.fields.pclass !== undefined)
+	test('Test filterByProperty', () => {
+		const allMale = data.filter(p => p.fields.sex === 'male')
+		const allFemale = data.filter(p => p.fields.sex === 'female')
 
-	expect(index.filterNullForProperty(data, 'fare')).toEqual(faresNotNull)
-	expect(index.filterNullForProperty(data, 'age')).toEqual(agesNotNull)
-	expect(index.filterNullForProperty(data, 'pclass')).toEqual(pclassNotNull)
-})
+		expect(index.filterByProperty(data, 'sex', 'male')).toEqual(allMale)
+		expect(index.filterByProperty(data, 'sex', 'female')).toEqual(allFemale)
+	})
 
-test('Test sumAllProperty', () => {
-	const sumAges = data.reduce((acc, p) => p.fields.age !== undefined ? acc + p.fields.age : acc, 0)
-	const sumFares = data.reduce((acc, p) => p.fields.fare !== undefined ? acc + p.fields.fare : acc, 0)
+	test('Test filterNullForProperty', () => {
+		const faresNotNull = data.filter(p => p.fields.fare !== undefined)
+		const agesNotNull = data.filter(p => p.fields.age !== undefined)
+		const pclassNotNull = data.filter(p => p.fields.pclass !== undefined)
 
-	expect(index.sumAllProperty(data, 'age')).toBe(sumAges)
-	expect(index.sumAllProperty(data, 'fare')).toBe(sumFares)
-})
+		expect(index.filterNullForProperty(data, 'fare')).toEqual(faresNotNull)
+		expect(index.filterNullForProperty(data, 'age')).toEqual(agesNotNull)
+		expect(index.filterNullForProperty(data, 'pclass')).toEqual(pclassNotNull)
+	})
 
-test('Test countAllProperty', () => {
-	const embarkedCounts = data.reduce((acc, p) => {
-		if (acc[p.fields.embarked] === undefined) {
-			acc[p.fields.embarked] = 1
-		} else {
-			acc[p.fields.embarked] += 1
-		}
-		return acc
-	}, {})
+	test('Test sumAllProperty', () => {
+		const sumAges = data.reduce((acc, p) => p.fields.age !== undefined ? acc + p.fields.age : acc, 0)
+		const sumFares = data.reduce((acc, p) => p.fields.fare !== undefined ? acc + p.fields.fare : acc, 0)
 
-	const genderCounts = data.reduce((acc, p) => {
-		if (acc[p.fields.sex] === undefined) {
-			acc[p.fields.sex] = 1
-		} else {
-			acc[p.fields.sex] += 1
-		}
-		return acc
-	}, {})
+		expect(index.sumAllProperty(data, 'age')).toBe(sumAges)
+		expect(index.sumAllProperty(data, 'fare')).toBe(sumFares)
+	})
 
-	const pclassCounts = data.reduce((acc, p) => {
-		if (acc[p.fields.pclass] === undefined) {
-			acc[p.fields.pclass] = 1
-		} else {
-			acc[p.fields.pclass] += 1
-		}
-		return acc
-	}, {})
+	test('Test countAllProperty', () => {
+		const embarkedCounts = data.reduce((acc, p) => {
+			if (acc[p.fields.embarked] === undefined) {
+				acc[p.fields.embarked] = 1
+			} else {
+				acc[p.fields.embarked] += 1
+			}
+			return acc
+		}, {})
 
-	expect(index.countAllProperty(data, 'embarked')).toEqual(embarkedCounts)
-	expect(index.countAllProperty(data, 'sex')).toEqual(genderCounts)
-	expect(index.countAllProperty(data, 'pclass')).toEqual(pclassCounts)
-})
+		const genderCounts = data.reduce((acc, p) => {
+			if (acc[p.fields.sex] === undefined) {
+				acc[p.fields.sex] = 1
+			} else {
+				acc[p.fields.sex] += 1
+			}
+			return acc
+		}, {})
 
-test('Test makeHistogram', () => {
+		const pclassCounts = data.reduce((acc, p) => {
+			if (acc[p.fields.pclass] === undefined) {
+				acc[p.fields.pclass] = 1
+			} else {
+				acc[p.fields.pclass] += 1
+			}
+			return acc
+		}, {})
 
-	const ages10 = data.filter(p => p.fields.age !== undefined).reduce((acc, p) => {
-		if (acc[Math.floor(p.fields.age / 10)] === undefined) {
-			acc[Math.floor(p.fields.age / 10)] = 1
-		} else {
-			acc[Math.floor(p.fields.age / 10)] += 1
-		}
-		return acc 
-	}, [])
+		expect(index.countAllProperty(data, 'embarked')).toEqual(embarkedCounts)
+		expect(index.countAllProperty(data, 'sex')).toEqual(genderCounts)
+		expect(index.countAllProperty(data, 'pclass')).toEqual(pclassCounts)
+	})
 
-	const ages5 = data.filter(p => p.fields.age !== undefined).reduce((acc, p) => {
-		if (acc[Math.floor(p.fields.age / 5)] === undefined) {
-			acc[Math.floor(p.fields.age / 5)] = 1
-		} else {
-			acc[Math.floor(p.fields.age / 5)] += 1
-		}
-		return acc 
-	}, [])
+	test('Test makeHistogram', () => {
 
-	const fares = data.filter(p => p.fields.age !== undefined).reduce((acc, p) => {
-		if (acc[Math.floor(p.fields.fare / 10)] === undefined) {
-			acc[Math.floor(p.fields.fare / 10)] = 1
-		} else {
-			acc[Math.floor(p.fields.fare / 10)] += 1
-		}
-		return acc 
-	}, [])
+		const ages10 = data.filter(p => p.fields.age !== undefined).reduce((acc, p) => {
+			if (acc[Math.floor(p.fields.age / 10)] === undefined) {
+				acc[Math.floor(p.fields.age / 10)] = 1
+			} else {
+				acc[Math.floor(p.fields.age / 10)] += 1
+			}
+			return acc 
+		}, [])
 
-	expect(index.makeHistogram(data, 'age', 10)).toEqual(Array.from(ages10, v => v || 0))
-	expect(index.makeHistogram(data, 'age', 5)).toEqual(Array.from(ages5, v => v || 0))
-	expect(index.makeHistogram(data, 'fare', 10)).toEqual(Array.from(fares, v => v || 0))
-})
+		const ages5 = data.filter(p => p.fields.age !== undefined).reduce((acc, p) => {
+			if (acc[Math.floor(p.fields.age / 5)] === undefined) {
+				acc[Math.floor(p.fields.age / 5)] = 1
+			} else {
+				acc[Math.floor(p.fields.age / 5)] += 1
+			}
+			return acc 
+		}, [])
 
-test('Test normalizeProperty', () => {
-	const ages = data.filter(p => p.fields.age !== undefined).map(p => p.fields.age)
-	const maxAge = Math.max(...ages)
-	const normalizedAges = ages.map(v => v / maxAge)
+		const fares = data.filter(p => p.fields.age !== undefined).reduce((acc, p) => {
+			if (acc[Math.floor(p.fields.fare / 10)] === undefined) {
+				acc[Math.floor(p.fields.fare / 10)] = 1
+			} else {
+				acc[Math.floor(p.fields.fare / 10)] += 1
+			}
+			return acc 
+		}, [])
 
-	const fares = data.map(p => p.fields.fare)
-	const maxFare = Math.max(...fares)
-	const normalizedFares = fares.map(v => v / maxFare)
-	
-	expect(index.normalizeProperty(data, 'age')).toEqual(normalizedAges)
-	expect(index.normalizeProperty(data, 'fare')).toEqual(normalizedFares)
+		expect(index.makeHistogram(data, 'age', 10)).toEqual(Array.from(ages10, v => v || 0))
+		expect(index.makeHistogram(data, 'age', 5)).toEqual(Array.from(ages5, v => v || 0))
+		expect(index.makeHistogram(data, 'fare', 10)).toEqual(Array.from(fares, v => v || 0))
+	})
+
+	test('Test normalizeProperty', () => {
+		const ages = data.filter(p => p.fields.age !== undefined).map(p => p.fields.age)
+		const maxAge = Math.max(...ages)
+		const normalizedAges = ages.map(v => v / maxAge)
+
+		const fares = data.map(p => p.fields.fare)
+		const maxFare = Math.max(...fares)
+		const normalizedFares = fares.map(v => v / maxFare)
+		
+		expect(index.normalizeProperty(data, 'age')).toEqual(normalizedAges)
+		expect(index.normalizeProperty(data, 'fare')).toEqual(normalizedFares)
+	})
+
 })
